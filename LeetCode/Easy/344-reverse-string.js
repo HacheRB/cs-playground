0462e8879b053f2e9ccaddcfd41dcee08fbe122c
Hector Rodriguez Bravo <61188167+HacheRB@users.noreply.github.com>
2021-08-13 22:51:43 +0100

    LeetCode 7 and 344

---
+++ LeetCode/Easy/344-reverse-string.js
@@ -0,0 +1,77 @@
+//https://leetcode.com/problems/reverse-string/
+
+// Solution #1 - .reverse()
+// Time Complexity O(n)
+// Space Complexity O(1)
+var solution = function(s) {
+    s= s.reverse()
+};
+
+//Solution #2 - for loop
+// Time Complexity O(n)
+// Space Complexity O(n)
+var solution2 = function(s) {
+    let reversed = []    
+    for (let char of s) {
+        reversed.unshift(char)
+    }
+    return reversed
+};
+
+//Solution #3 - reduce, not in place
+// Time Complexity O(n)
+// Space Complexity O(1)
+var solution3 = function(s) {
+    return s.reduce((rev, char) => char + rev, ''). split('')    
+};
+
+//Solution #4 - 2 pointers
+// Time Complexity O(n)
+// Space Complexity O(1)
+var solution4 = function(s) {
+    let left = 0
+    let right = s.length-1    
+    while (left < right) {
+        let temp = s[left];
+        s[left++]= s[right]
+        s[right--] = temp
+    }
+    return s
+};
+
+let s = ["h","e","l","l","o"]
+
+console.log('Solution #1')
+const t0 = performance.now();
+console.log(solution(s))
+const t1 = performance.now();
+console.log(`Took ${t1 - t0} milliseconds.`);
+console.log('--------------------------------------')
+
+s = ["h","e","l","l","o"]
+
+console.log('Solution #2')
+const t2 = performance.now();
+console.log(solution2(s))
+const t3 = performance.now();
+console.log(`Took ${t3 - t2} milliseconds.`);
+console.log('--------------------------------------')
+
+s = ["h","e","l","l","o"]
+
+console.log('Solution #3')
+const t4 = performance.now();
+console.log(solution3(s))
+const t5 = performance.now();
+console.log(`Took ${t5 - t4} milliseconds.`);
+console.log('--------------------------------------')
+
+console.log('Solution #4')
+const t6 = performance.now();
+console.log(solution4(s))
+const t7 = performance.now();
+console.log(`Took ${t7 - t6} milliseconds.`);
+console.log('--------------------------------------')
+
+
+
---
+++ LeetCode/Easy/7-reverse-integer.js
@@ -0,0 +1,22 @@
+// LeetCode Link
+
+// Solution #1
+// Time Complexity - O(n)?
+// Space Complexity O(n)
+var solution1 = function(x) {
+  let reversed = x.toString().split('').reverse().join('')
+  let parsed = parseInt(reversed) * Math.sign(x)
+  if(parsed <= Math.pow(-2,31) || parsed >= Math.pow(2,31)-1 ){
+    return 0
+  } else return parsed
+};
+
+
+let x = 1534236469
+console.log('Solution #1')
+const t0 = performance.now();
+console.log(solution1(x))
+const t1 = performance.now();
+console.log(`Took ${t1 - t0} milliseconds.`);
+console.log('--------------------------------------')
+
