624061fe9c74632dbbacdfa9db9f2f58442b2117
Hector Rodriguez Bravo <61188167+HacheRB@users.noreply.github.com>
2021-08-18 18:50:13 +0100

    LeetCode 242

---
+++ LeetCode/Easy/242-valid-anagram.js
@@ -0,0 +1,54 @@
+// https://leetcode.com/problems/valid-anagram/
+
+// Solution #1
+// Time Complexity
+// Space Complexity
+var solution1 = function (s, t) {
+  let sHashMap = buildHashMap(s)
+  let tHashMap = buildHashMap(t)
+
+  if (Object.keys(sHashMap).length !== Object.keys(tHashMap).length) {
+    return false
+  }
+
+  for (let char in sHashMap) {
+    if (sHashMap[char] !== tHashMap[char]) {
+      return false
+    }
+  }
+  return true
+}
+
+//Solution #2
+// Time Complexity
+// Space Complexity
+var solution2 = function (s, t) {
+  if()
+  return s.split('').sort().join('') === t.split('').sort().join('')
+}
+
+let s = 'rat'
+let t = 'car'
+
+function buildHashMap(string) {
+  const hashMap = {}
+  for (let char of string) {
+    hashMap[char] = hashMap[char] +1 || 1
+  }
+  console.log('hashmap', hashMap)
+  return hashMap
+}
+
+console.log('Solution #1')
+const t0 = performance.now()
+console.log(solution1(s, t))
+const t1 = performance.now()
+console.log(`Took ${t1 - t0} milliseconds.`)
+console.log('--------------------------------------')
+
+console.log('Solution #2')
+const t2 = performance.now()
+console.log(solution2(s, t))
+const t3 = performance.now()
+console.log(`Took ${t3 - t2} milliseconds.`)
+console.log('--------------------------------------')
