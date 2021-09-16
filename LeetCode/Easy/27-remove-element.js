4b9e959db43dde6b028d2facdb8968fe2010b6a0
Hector Rodriguez Bravo <61188167+HacheRB@users.noreply.github.com>
2021-09-16 21:15:18 +0100

    Leetcode 27

---
+++ LeetCode/Easy/27-remove-element.js
@@ -0,0 +1,49 @@
+// https://leetcode.com/problems/remove-element/
+
+// Solution #1
+// Time Complexity O(n^2)
+// Space Complexity O(1)
+
+var solution1 = function (nums, val) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1)
+      i--
+    }
+  }
+  return nums.length
+}
+
+// Solution #2 - In place
+// Time Complexity O(n)
+// Space Complexity O(1)
+
+var solution2 = function (nums, val) {
+  let i = 0
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== val) {
+      nums[i] = nums[j]
+      i++
+    }
+  }
+  return i
+}
+
+let nums = [0, 1, 2, 2, 3, 0, 4, 2]
+let val = 2
+
+console.log('Solution #1')
+const t0 = performance.now()
+console.log(solution1(nums, val))
+const t1 = performance.now()
+console.log(`Took ${t1 - t0} milliseconds.`)
+console.log('--------------------------------------')
+
+let nums2 = [0, 1, 2, 2, 3, 0, 4, 2]
+
+console.log('Solution #2')
+const t2 = performance.now()
+console.log(solution2(nums2, val))
+const t3 = performance.now()
+console.log(`Took ${t3 - t2} milliseconds.`)
+console.log('--------------------------------------')
--- LeetCode/template.js
+++ LeetCode/template.js
@@ -1,45 +1,37 @@
 // LeetCode Link
 
 // Solution #1
-// Time Complexity 
-// Space Complexity 
-var solution1 = function() {
-   };
+// Time Complexity O()
+// Space ComplexityO()
+var solution1 = function () {}
 
 //Solution #2
-// Time Complexity 
-// Space Complexity 
-var solution2 = function() {   
-};
+// Time Complexity O()
+// Space ComplexityO()
+var solution2 = function () {}
 
 //Solution #3
-// Time Complexity O
-// Space Complexity 
-var solution3 = function() {
-};
-
+// Time Complexity O()
+// Space ComplexityO()
+var solution3 = function () {}
 
 console.log('Solution #1')
-const t0 = performance.now();
+const t0 = performance.now()
 console.log(solution1())
-const t1 = performance.now();
-console.log(`Took ${t1 - t0} milliseconds.`);
+const t1 = performance.now()
+console.log(`Took ${t1 - t0} milliseconds.`)
 console.log('--------------------------------------')
-
 
 console.log('Solution #2')
-const t2 = performance.now();
+const t2 = performance.now()
 console.log(solution2())
-const t3 = performance.now();
-console.log(`Took ${t3 - t2} milliseconds.`);
+const t3 = performance.now()
+console.log(`Took ${t3 - t2} milliseconds.`)
 console.log('--------------------------------------')
-
 
 console.log('Solution #3')
-const t4 = performance.now();
+const t4 = performance.now()
 console.log(solution3())
-const t5 = performance.now();
-console.log(`Took ${t5 - t4} milliseconds.`);
+const t5 = performance.now()
+console.log(`Took ${t5 - t4} milliseconds.`)
 console.log('--------------------------------------')
-
-
