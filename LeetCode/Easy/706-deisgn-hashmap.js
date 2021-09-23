20dccb219903f8160d73f04ab1a6052804c0dab6
Hector Rodriguez Bravo <61188167+HacheRB@users.noreply.github.com>
2021-09-23 19:04:16 +0100

    Leetcode 706

---
+++ LeetCode/Easy/706-deisgn-hashmap.js
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/design-hashmap/
+
+// Solution #1
+// Time Complexity O()
+// Space Complexity O()
+var solution1 = function () {
+  this.data = new Array(1000001)
+}
+
+MyHashMap.prototype.put = function (key, value) {
+  this.data[key] = value
+}
+
+MyHashMap.prototype.get = function (key) {
+  let value = this.data[key]
+  return value !== undefined ? value : -1
+}
+
+MyHashMap.prototype.remove = function (key) {
+  delete this.data[key]
+}
+
+console.log('Solution #1')
+const t0 = performance.now()
+console.log(solution1())
+const t1 = performance.now()
+console.log(`Took ${t1 - t0} milliseconds.`)
+console.log('--------------------------------------')
