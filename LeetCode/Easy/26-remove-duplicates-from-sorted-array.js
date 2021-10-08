bab61ef1b91a4e3bc4255af3837817c53971c8b7
Hector Rodriguez Bravo <61188167+HacheRB@users.noreply.github.com>
2021-10-08 12:04:14 +0100

    Leetcode 26

--- LeetCode/Easy/26-remove-duplicates-from-sorted-array.js
+++ LeetCode/Easy/26-remove-duplicates-from-sorted-array.js
@@ -1,10 +1,11 @@
 // https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 
-// Solution #1
-// Time Complexity O()
-// Space ComplexityO()
+// Solution #1 - In Place Two Pointers
+// Time Complexity O(n)
+// Space Complexity O(1)
+
 var solution1 = function (nums) {
-  let insertIndex = 1
+  let insertIndex = 1 // Slow runner
   if (nums.length === 0) return 0
 
   for (let i = 1; i < nums.length; i++) {
@@ -16,16 +17,6 @@ var solution1 = function (nums) {
   return insertIndex
 }
 
-//Solution #2
-// Time Complexity O()
-// Space ComplexityO()
-var solution2 = function () {}
-
-//Solution #3
-// Time Complexity O()
-// Space ComplexityO()
-var solution3 = function () {}
-
 let nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
 
 console.log('Solution #1')
@@ -33,18 +24,4 @@ const t0 = performance.now()
 console.log(solution1(nums))
 const t1 = performance.now()
 console.log(`Took ${t1 - t0} milliseconds.`)
-console.log('--------------------------------------')
-
-console.log('Solution #2')
-const t2 = performance.now()
-console.log(solution2())
-const t3 = performance.now()
-console.log(`Took ${t3 - t2} milliseconds.`)
-console.log('--------------------------------------')
-
-console.log('Solution #3')
-const t4 = performance.now()
-console.log(solution3())
-const t5 = performance.now()
-console.log(`Took ${t5 - t4} milliseconds.`)
 console.log('--------------------------------------')
