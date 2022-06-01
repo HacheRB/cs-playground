7842db5d7a256c6db60cea2b899c2c1d0c7c6eed
Héctor Rodríguez Bravo <61188167+HacheRB@users.noreply.github.com>
2022-06-01 19:30:52 +0100

    Adjust console.log

--- Data-Structures/queue.js
+++ Data-Structures/queue.js
@@ -37,13 +37,9 @@ let q = new Queue()
 for (let i = 1; i <= 7; i++) {
 	q.enqueue(i)
 }
-// get the current item at the front of the queue
-console.log(q.peek()) // 1
+console.log(q.peek())
+console.log(q.length)
 
-// get the current length of queue
-console.log(q.length) // 7
-
-// dequeue all elements
 while (!q.isEmpty) {
 	console.log(q.dequeue())
 }
